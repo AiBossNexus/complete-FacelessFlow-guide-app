02fff20975501312f9ec41a0963e90f70c150c65
AiBoss <moshekon2@gmail.com>
2026-05-07 10:44:46 +0300

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './complete-FacelessFlow-guide-app',//
+  base: '/complete-FacelessFlow-guide-app/',
 })
